449f6d14574a731543167fbc0e72a09dd4e03d4c
Brian Hill <hillbr18@gmail.com>
2026-06-24 20:32:02 -0400

    Fixed text size on about section for mobile to make reading easier

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -16,8 +16,8 @@
   		</div>
 
   		<div class="mt-6 lg:mt-0 lg:pl-16 text-center lg:text-left">
-    		<h3 class="text-3xl font-mono font-bold inline-block">Who am I?</h3>
-    		<p class="about_desc text-lg font-mono mt-2 mb-8">
+    		<h3 class="text-2xl sm:text-3xl font-mono font-bold inline-block">Who am I?</h3>
+    		<p class="about_desc text-base sm:text-lg font-mono mt-2 mb-8">
 				Hi, I'm Brian Hill, a Software Developer and Computer Engineer with a focus on embedded systems and hardware design. I spend a lot of time outside of work/school doing projects I find fun - which is what ends up here. All of my projects are on Github and if you have any specific questions, shoot me an email.
 			</p>
     		<a href="/bhill_resume.pdf" class="button bg-black text-neutral-100 inline-block px-6 py-2 font-mono text-sm font-bold transition-colors duration-200" target="_blank" rel="noopener noreferrer">Resume PDF</a>
